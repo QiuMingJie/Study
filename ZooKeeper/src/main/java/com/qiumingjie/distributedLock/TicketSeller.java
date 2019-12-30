b43b7bd3dec03672e5a5b502d061f23223d38a71
DESKTOP-K6VQ42D\Qiu <838689269@qq.com>
2019-12-30 15:27:35 +0800

    学习ZooKeeper

---
+++ ZooKeeper/src/main/java/com/qiumingjie/distributedLock/TicketSeller.java
@@ -0,0 +1,39 @@
+package com.qiumingjie.distributedLock;
+
+import org.apache.zookeeper.KeeperException;
+
+import java.io.IOException;
+
+/**
+ * @author QiuMingJie
+ * @date 2019-12-30 15:20
+ * @Description 售票
+ */
+public class TicketSeller {
+    private void sell(){
+        System.out.println("售票开始");
+        // 线程随机休眠数毫秒，模拟现实中的费时操作
+        int sleepMillis = (int) (Math.random() * 2000);
+        try {
+            //代表复杂逻辑执行了一段时间
+            Thread.sleep(sleepMillis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("售票结束");
+    }
+
+    public void sellTicketWithLock() throws KeeperException, InterruptedException, IOException {
+        LockSample lock = new LockSample();
+        lock.acquireLock();
+        sell();
+        lock.releaseLock();
+    }
+
+    public static void main(String[] args) throws KeeperException, InterruptedException, IOException {
+        TicketSeller ticketSeller = new TicketSeller();
+        for(int i=0;i<1000;i++){
+            ticketSeller.sellTicketWithLock();
+        }
+    }
+}
